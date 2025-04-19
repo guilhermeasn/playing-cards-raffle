9a00521b955f1b9ec23a5af285d876b168ed40b5
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-04-19 14:53:37 -0300

    init toPlay

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * ## Suit (Naipes)
  * - ♠: Spade (Espadas) - BLACK 
@@ -6,6 +5,40 @@
  * - ♣: Club (Paus) - BLACK 
  * - ♥: Heart (Copas) - RED 
  */
+type Suit = '♠' | '♦' | '♣' | '♥';
+
+type BetChips = {
+    chips: number;
+    increase: number;
+}
+
+type BetOption = (
+    | 'red'
+    | 'black'
+    | 'oneSpade'
+    | 'oneDiamond'
+    | 'oneClub'
+    | 'oneHeart'
+    | 'points3to19'
+    | 'points20to30'
+);
+
+type Bet = {
+    chips: number;
+    raffles: number;
+} & Record<BetOption, BetChips>;
+
+const award : Record<BetOption, number> = {
+    red: 1.9,
+    black: 1.9,
+    oneSpade: 2.06,
+    oneDiamond: 2.06,
+    oneClub: 2.06,
+    oneHeart: 2.06,
+    points3to19: 2,
+    points20to30: 1.81,
+}
+
 const cards = [
     '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', 'T♠', 'J♠', 'Q♠', 'K♠', 'A♠',
     '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', 'T♦', 'J♦', 'Q♦', 'K♦', 'A♦',
@@ -43,22 +76,86 @@ const color = (cards: string[]) : 'BLACK' | 'RED' | 'NEUTRAL' => {
 
 };
 
-const main = () => {
+const amount = (cards: string[]) : Record<Suit, number> => {
 
-    let chips : number = 30000;
-    const bet : number = 200;
-    
-    while (chips > 0) {
+    const suit : Record<Suit, number> = {
+        '♠': 0, '♦': 0, '♣': 0, '♥': 0
+    }
 
-        chips -= bet;
+    cards.forEach(card => {
+        suit[card.charAt(1) as Suit]++;
+    });
 
-        const r = raffle();
-        const c = color(r);
-        
-        if(c === 'RED') chips += bet * 1.8;
+    return suit;
 
-        console.log(r, c, chips);
+}
+
+const points = (cards: string[]) : number => {
+
+    let sum : number = 0;
+
+    cards.forEach(card => {
+        switch(card.charAt(0)) {
+            case 'A': sum += 1; break;
+            case '2': sum += 2; break;
+            case '3': sum += 3; break;
+            case '4': sum += 4; break;
+            case '5': sum += 5; break;
+            case '6': sum += 6; break;
+            case '7': sum += 7; break;
+            case '8': sum += 8; break;
+            case '9': sum += 9; break;
+            default: sum += 10; break;
+        }
+    });
+
+    return sum;
+
+}
+
+const toPlay = (bet : Bet) : number => {
+
+    let chips = bet.chips;
+
+    for(let r = 0; r < bet.raffles; r++) {
         
     }
 
+    return chips;
+
 }
+
+const main = () => {
+
+    let chips : number = 30000;
+    let errors : number = 0;
+    const bet : number = 200;
+
+    let a : number = 0;
+    
+    while (chips > 0) {
+
+        a++;
+
+        const r = raffle();
+        const c = color(r);
+        const b = bet * (errors + 1);
+        
+        chips -= bet;
+        
+        if(c === 'RED') {
+            chips += bet * 1.9;
+            errors = 0;
+        } else {
+            errors > 1 ? errors = 0 : errors++;
+        }
+
+        console.log(r, c, amount(r), points(r), b, chips);
+
+    }
+
+    console.log(a);
+
+}
+
+main();
